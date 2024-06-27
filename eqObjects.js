2adff372e5040e59ced7977b14311839aebac85c
Rohan Kulkarni <irohunk@gmail.com>
2024-06-27 11:47:32 -0400

    updated based on Larry's inputs

--- eqObjects.js
+++ eqObjects.js
@@ -7,25 +7,45 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  
+  let result = true;
+  
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      result = false;
+    }
+  } return result;
+};
+
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function(object1, object2) {
   const objKeys1 = Object.keys(object1);
-  objKeys1.sort();
   const objKeys2 = Object.keys(object2);
-  objKeys2.sort();
-
+  
   if (objKeys1.length !== objKeys2.length) {
     return false;
   }
 
-  for (let i = 0; i < objKeys1.length; i++) {
-    if (objKeys1[i] !== objKeys2[i] || object1[objKeys1[i]] !== object2[objKeys2[i]]) {
+  for (let key of objKeys1) {
+    const val1 = object1[key];
+    const val2 = object2[key];
+
+    if (Array.isArray(val1) && Array.isArray(val2)) {
+      if (!eqArrays(val1, val2)) {
+        return false;
+      }
+    } else if (val1 !== val2) {
       return false;
     }
-  }
-  return true;
+  } return true;
 };
+  
+
 
 
 
@@ -38,3 +58,14 @@ assertEqual((testObj), true);
 const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 testObj = eqObjects(shirtObject, longSleeveShirtObject); // => false
 assertEqual((testObj), false);
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true); // => true
+
+const longSleeveMultiColorShirtObject = {
+  size: "medium",
+  colors: ["red", "blue"],
+  sleeveLength: "long",
+};
+assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false); // => false
